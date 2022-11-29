7143b3106a40867b5cd5174d6ee1f263735d09df
unknown <4034342+rodneysantos@users.noreply.github.com>
2022-11-30 00:49:17 +0800

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: 'portfolio',
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Portfolio",
+    siteUrl: "https://www.yourdomain.tld"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
