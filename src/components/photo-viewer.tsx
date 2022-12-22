3ac85b8da501bdc361da345bc99a5ff756b49bbe
rodneysantos <4034342+rodneysantos@users.noreply.github.com>
2022-12-22 22:22:10 +0800

    test(photo-viewer): add unit tests

--- src/components/photo-viewer.tsx
+++ src/components/photo-viewer.tsx
@@ -15,10 +15,10 @@ const PhotoViewer: React.FC<PhotoViewerProps> = ({
   return (
     <>
       {isVisible && (
-        <div className={css.photoViewer__background} onClick={() => closeHandler()}>
+        <div className={css.photoViewer__background} onClick={() => closeHandler()} data-testid="photo-viewer">
           <div className={css.photoViewer__wrapper}>
-            <span className={css.photoViewer__close}>Close</span>
-            <img className={css.photoViewer__img} src={src} alt="" />
+            <span className={css.photoViewer__close} data-testid="photo-viewer-close">Close</span>
+            <img className={css.photoViewer__img} src={src} alt="" data-testid="photo-viewer-img" />
           </div>
         </div>
       )}
